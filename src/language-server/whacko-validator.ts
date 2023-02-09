d2764dd0824b33799787496bbd508cb5f47a92ff
jtenner <tenner.joshua@gmail.com>
2023-02-09 00:50:31 -0500

    first commit. what should be ignored?

---
+++ src/language-server/whacko-validator.ts
@@ -0,0 +1,31 @@
+import { ValidationAcceptor, ValidationChecks } from 'langium';
+import { WhackoAstType, Person } from './generated/ast';
+import type { WhackoServices } from './whacko-module';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: WhackoServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.WhackoValidator;
+    const checks: ValidationChecks<WhackoAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class WhackoValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
