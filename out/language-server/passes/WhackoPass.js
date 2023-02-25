e40ff55e2c342b12a3eb03ea693edea845afd8c5
CountBleck <Mr.YouKnowWhoIAm@protonmail.com>
2023-02-24 18:54:19 -0800

    Remove and ignore out/ directory from Git

--- out/language-server/passes/WhackoPass.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WhackoPass = void 0;
-const WhackoVisitor_1 = require("../WhackoVisitor");
-class WhackoPass extends WhackoVisitor_1.WhackoVisitor {
-    diagnostics = [];
-}
-exports.WhackoPass = WhackoPass;
-//# sourceMappingURL=WhackoPass.js.map
